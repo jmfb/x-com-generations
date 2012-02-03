42e02f47bf7e00a602a2cfaed176f971ec98721e
Jacob Buysse <Jacob.Buysse@gmail.com>
2012-02-03 02:03:27 -0600

    Some comments and png files for test map squares.
    Signed-off-by: Jacob Buysse <Jacob.Buysse@gmail.com>

--- Screens/ScreenTestBattle.cpp
+++ Screens/ScreenTestBattle.cpp
@@ -5,6 +5,53 @@
 namespace XCom
 {
 
+
+
+class BattleMapSquare
+{
+public:
+	//Load the square stuffs
+	void Load(int file);
+	//TODO: a single square might have an animation, thus multipls images
+	//TODO: file format form square images (reuse xmg, need to recreate editor)
+	//TODO: probably good to create converter
+	//NOTE: I would prefer to fix the Y-axis ambiguities prior to working
+	//		on further images.
+};
+
+class BattleMapSection
+{
+public:
+	//Load a predefined section from file
+	void Load(int file);
+	
+	//Retrieve one of the predefined map sections
+	static BattleMapSection& Get(int index);
+	
+	//TODO: more (squares, levels, etc)
+};
+
+class BattleMap
+{
+public:
+	//Generate a new battle map
+	void Generate(int terrain);
+	void GenerateAlienBase(int age);
+	void GenerateXComBase(int xcombase);
+	
+	//Save the state of the map when saving during a battle
+	void Save(int file) const;
+	
+	//Load the state of the file from a previous save
+	void Load(int file);
+	
+	//TODO: more (section counts, sections, etc)
+};
+
+
+
+
+
 ScreenTestBattle::ScreenTestBattle()
 {
 }
